9101619149ff18d981887a53b3e0e0a5e8700c12
Hamnas173 <hamnasakhawat678@gmail.com>
2024-06-27 20:08:25 +0500

    Updates

---
+++ precache-manifest.c0ef9d1d2328e152d6f1f186a3961b18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6a2f47b135ea795687fcc66f560d258",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "e43c12f5b7ba32d5d983",
+    "url": "/TextUtils/static/css/main.3754177f.chunk.css"
+  },
+  {
+    "revision": "178a5f007b6a2c7c6a20",
+    "url": "/TextUtils/static/js/2.12d3a3ae.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/TextUtils/static/js/2.12d3a3ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c22086003eed385c50",
+    "url": "/TextUtils/static/js/3.b5bd36bd.chunk.js"
+  },
+  {
+    "revision": "e43c12f5b7ba32d5d983",
+    "url": "/TextUtils/static/js/main.f4349133.chunk.js"
+  },
+  {
+    "revision": "f41fec5e52ab17bc18cc",
+    "url": "/TextUtils/static/js/runtime-main.f951074a.js"
+  }
+]);
